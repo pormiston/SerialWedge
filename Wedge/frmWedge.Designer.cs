f5430e39c3eaebd07b2da1eb40837057c93d060f
Paul Ormiston <pormiston@shoplogix.com>
2012-03-13 11:57:18 -0400

    Initial Commit

---
+++ Wedge/frmWedge.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Wedgies
+{
+    partial class frmWedge
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmWedge));
+            this.cboPort = new System.Windows.Forms.ComboBox();
+            this.cboBaudRate = new System.Windows.Forms.ComboBox();
+            this.chkOnOff = new System.Windows.Forms.CheckBox();
+            this.bgwInterceptWorker = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // cboPort
+            // 
+            this.cboPort.FormattingEnabled = true;
+            this.cboPort.Location = new System.Drawing.Point(42, 36);
+            this.cboPort.Name = "cboPort";
+            this.cboPort.Size = new System.Drawing.Size(121, 21);
+            this.cboPort.TabIndex = 0;
+            // 
+            // cboBaudRate
+            // 
+            this.cboBaudRate.FormattingEnabled = true;
+            this.cboBaudRate.Location = new System.Drawing.Point(42, 83);
+            this.cboBaudRate.Name = "cboBaudRate";
+            this.cboBaudRate.Size = new System.Drawing.Size(121, 21);
+            this.cboBaudRate.TabIndex = 1;
+            // 
+            // chkOnOff
+            // 
+            this.chkOnOff.Appearance = System.Windows.Forms.Appearance.Button;
+            this.chkOnOff.AutoSize = true;
+            this.chkOnOff.Location = new System.Drawing.Point(80, 144);
+            this.chkOnOff.Name = "chkOnOff";
+            this.chkOnOff.Size = new System.Drawing.Size(50, 23);
+            this.chkOnOff.TabIndex = 2;
+            this.chkOnOff.Text = "On/Off";
+            this.chkOnOff.UseVisualStyleBackColor = true;
+            // 
+            // frmWedge
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(203, 215);
+            this.Controls.Add(this.chkOnOff);
+            this.Controls.Add(this.cboBaudRate);
+            this.Controls.Add(this.cboPort);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmWedge";
+            this.Text = "Wedge";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cboPort;
+        private System.Windows.Forms.ComboBox cboBaudRate;
+        private System.Windows.Forms.CheckBox chkOnOff;
+        private System.ComponentModel.BackgroundWorker bgwInterceptWorker;
+    }
+}
+
---
+++ Wedge/frmWedge.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+
+namespace Wedgies
+{
+    public partial class frmWedge : Form
+    {
+        private int[] bauds = {110, 300, 600, 1200, 2400, 4800, 9600, 14400, 19200, 38400, 57600};
+        private bool bRunning = false;
+        private SerialPort port = null;
+
+        public frmWedge()
+        {
+            InitializeComponent();
+
+            Populate();
+
+            port = new SerialPort();
+            port.ReadTimeout = 500;
+            port.WriteTimeout = 500; 
+
+            //handle events
+            chkOnOff.CheckedChanged += new EventHandler(chkOnOff_CheckedChanged);
+            FormClosing += new FormClosingEventHandler(frmWedge_FormClosing);
+            bgwInterceptWorker.DoWork += new DoWorkEventHandler(interceptBuffer);
+            bgwInterceptWorker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(StartStop);
+        }
+
+        void frmWedge_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            chkOnOff.Checked = false;
+        }
+
+        void chkOnOff_CheckedChanged(object sender, EventArgs e)
+        {
+            if (bRunning)bRunning = false; // this will will cause the Completed event to fire so we don't have to call StartStop here
+            else StartStop(null, null); 
+        }
+
+        private void ToggleForm(bool enable)
+        {
+            cboBaudRate.Enabled = cboPort.Enabled = enable;
+        }
+
+        private void Populate()
+        {
+            cboPort.DataSource = SerialPort.GetPortNames();
+            cboBaudRate.DataSource = bauds;
+        }
+        private void StartStop(object sender, RunWorkerCompletedEventArgs args)
+        {
+            ToggleForm(!chkOnOff.Checked);
+            
+            if (port.IsOpen) port.Close();
+           
+            if (chkOnOff.Checked)
+            {
+                port.PortName = cboPort.SelectedItem + "";
+                port.BaudRate = (int)cboBaudRate.SelectedItem;
+                port.Open();
+                
+                bRunning = true;
+                bgwInterceptWorker.RunWorkerAsync();
+            }
+        }
+        private void interceptBuffer(object sender, DoWorkEventArgs args)
+        {
+            while (bRunning)
+            {
+                try
+                {
+                    var line = port.ReadLine();
+                    SendKeys.SendWait(line);
+                }
+                catch (TimeoutException) { }
+            }
+        }
+
+    }
+}
